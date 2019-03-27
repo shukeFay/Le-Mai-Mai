101f627b1150c443c29b4a773d369aa3b4d3647a
shukeFay <826219314@qq.com>
2019-03-27 11:24:33 +0800

    乐麦麦1.0版,头部尾部整合

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//引入全局样式
+import style from './assets/statics/site/css/style.css';
 
 new Vue({
   render: h => h(App),
